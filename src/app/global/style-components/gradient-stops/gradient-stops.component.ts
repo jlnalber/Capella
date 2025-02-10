991b6350246a6ead14ec7735a2ed21091e532782
Julian Henrik Alber <alber.julian@outlook.com>
2025-02-10 21:30:44 +0100

    replace more icons

--- src/app/global/style-components/gradient-stops/gradient-stops.component.ts
+++ src/app/global/style-components/gradient-stops/gradient-stops.component.ts
@@ -2,15 +2,35 @@ import { Component, Input } from '@angular/core';
 import Picker from '../pickers/picker';
 import AbstractPickerComponent from '../abstractPickerComponent';
 import { GradientColorStop } from '../../interfaces/canvasStyles/colorStyle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-gradient-stops',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './gradient-stops.component.html',
   styleUrl: './gradient-stops.component.scss'
 })
 export class GradientStopsComponent extends AbstractPickerComponent<Picker<GradientColorStop[]>, GradientColorStop[]> {
   @Input({required: true}) public picker?: Picker<GradientColorStop[]>;
+
+  public addStop(): void {
+    if (this.picker !== undefined && this.picker.value === undefined) {
+      this.picker.value = [];
+    }
+    if (this.picker !== undefined && this.picker.value !== undefined) {
+      this.picker.value.push([0, {r: 0, g: 0, b: 0, a: 1}]);
+      this.picker.triggerChange();
+    }
+  }
+
+  public removeStop(index: number): void {
+    if (this.picker !== undefined && this.picker.value !== undefined) {
+      this.picker.value.splice(index, 1);
+      this.picker.triggerChange();
+    }
+  }
 
 }

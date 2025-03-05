84b4b4b4c36cdd7dc17adc2c2154715238501766
Julian Henrik Alber <alber.julian@outlook.com>
2025-03-05 19:42:53 +0100

    add new settings for pens

--- src/app/global/style-components/easy-pen-style-style/easy-pen-style-style.component.ts
+++ src/app/global/style-components/easy-pen-style-style/easy-pen-style-style.component.ts
@@ -1,17 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import Picker from '../pickers/picker';
 import { EasyPenStyle } from 'src/app/whiteboard/global/interfaces/penStyle';
 import AbstractPickerComponent from '../abstractPickerComponent';
+import { EasyStrokeStyleComponent } from "../easy-stroke-style/easy-stroke-style.component";
+import { EasyStrokeStyle } from '../../interfaces/canvasStyles/strokeStyle';
+import { ObjectStyleComponent } from "../object-style/object-style.component";
+import ObjectStyle from '../../interfaces/canvasStyles/objectStyle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-easy-pen-style-style',
   standalone: true,
-  imports: [],
+  imports: [
+    EasyStrokeStyleComponent,
+    ObjectStyleComponent,
+    FormsModule
+  ],
   templateUrl: './easy-pen-style-style.component.html',
   styleUrl: './easy-pen-style-style.component.scss'
 })
-export class EasyPenStyleStyleComponent extends AbstractPickerComponent<Picker<EasyPenStyle>, EasyPenStyle> {
+export class EasyPenStyleStyleComponent extends AbstractPickerComponent<Picker<EasyPenStyle>, EasyPenStyle> implements AfterViewInit {
 
   @Input({required: true}) public picker?: Picker<EasyPenStyle>;
+
+  
+  public easyStrokeStylePicker?: Picker<EasyStrokeStyle>;
+  public objectStylePicker?: Picker<ObjectStyle>;
+  
+  ngAfterViewInit() {
+    this.easyStrokeStylePicker = new Picker<EasyStrokeStyle>(() => this.picker?.value?.strokeStyle, (style?: EasyStrokeStyle) => {
+      if (style && this.picker !== undefined && this.picker.value !== undefined) {
+        this.picker.value.strokeStyle = style;
+        this.picker.triggerChange();
+      }
+    }, true, () => this.picker?.isDisabled() ?? false);
+    this.easyStrokeStylePicker.onValueChanged.addListener(() => {
+        this.onChange();
+    })
+
+    this.objectStylePicker = new Picker<ObjectStyle>(() => this.picker?.value?.objectStyle, (style?: ObjectStyle) => {
+      if (style && this.picker !== undefined && this.picker.value !== undefined) {
+        this.picker.value.objectStyle = style;
+        this.picker.triggerChange();
+      }
+    }, true, () => this.picker?.isDisabled() ?? false);
+    this.objectStylePicker.onValueChanged.addListener(() => {
+        this.onChange();
+    })
+  }
 
 }

07236a96a770949bbc56afd83ec39e8906e0cff0
Julian Henrik Alber <alber.julian@outlook.com>
2025-03-12 13:51:16 +0100

    reduce code

--- src/app/whiteboard/settings/edit-pen-quick-actions/edit-pen-quick-actions.component.ts
+++ src/app/whiteboard/settings/edit-pen-quick-actions/edit-pen-quick-actions.component.ts
@@ -3,20 +3,25 @@ import { WhiteboardService } from '../../services/whiteboard.service';
 import { Pen } from '../../global/interfaces/penStyle';
 import { FormsModule } from '@angular/forms';
 import AbstractSettingsComponent from '../abstractSettingComponent';
+import PenPicker from 'src/app/global/style-components/pickers/penPicker';
+import { ChoosePenComponent } from "../../../global/style-components/choose-pen/choose-pen.component";
+
+type NumAndPicker = { num: number, picker?: PenPicker };
 
 @Component({
   selector: 'app-edit-pen-quick-actions',
   standalone: true,
   imports: [
-    FormsModule
-  ],
+    FormsModule,
+    ChoosePenComponent
+],
   templateUrl: './edit-pen-quick-actions.component.html',
   styleUrl: './edit-pen-quick-actions.component.scss'
 })
 export class EditPenQuickActionsComponent extends AbstractSettingsComponent implements OnDestroy {
 
   add() {
-    this.order.push(0);
+    this.order.push(this.getNumAndPicker(0));
   }
 
   removePen(i: number) {
@@ -24,20 +29,28 @@ export class EditPenQuickActionsComponent extends AbstractSettingsComponent impl
   }
 
   public save() {
-    this.whiteboardService.settings.setPensOrder(this.order);
+    this.whiteboardService.settings.setPensOrder(this.order.map(i => i.num));
   }
 
   public pens: Pen[];
-  public order: number[];
+  public order: { num: number, picker?: PenPicker }[];
 
   constructor(public readonly whiteboardService: WhiteboardService) {
     super();
     this.pens = this.whiteboardService.settings.getPens();
-    this.order = this.whiteboardService.settings.getPensOrder().filter(i => i < this.pens.length);
+    this.order = this.whiteboardService.settings.getPensOrder().filter(i => i < this.pens.length).map(i => this.getNumAndPicker(i));
   }
 
   ngOnDestroy(): void {
       this.closed = true;
+  }
+
+  private getNumAndPicker(i: number): NumAndPicker {
+    const val: NumAndPicker = {
+      num: i
+    }
+    val.picker = new PenPicker(() => val.num, (v) => val.num = v ?? val.num, this.pens, true);
+    return val;
   }
   
 }

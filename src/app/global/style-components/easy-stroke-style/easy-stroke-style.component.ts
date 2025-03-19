7ca40535f5e7a5a4834f36f0b9b9683a0d743696
Julian Henrik Alber <alber.julian@outlook.com>
2025-03-19 10:52:15 +0100

    update easy stroke style

--- src/app/global/style-components/easy-stroke-style/easy-stroke-style.component.ts
+++ src/app/global/style-components/easy-stroke-style/easy-stroke-style.component.ts
@@ -1,17 +1,17 @@
 import { Component, Input } from '@angular/core';
 import Picker from '../pickers/picker';
-import { EasyStrokeStyle, getEmptyEasyStrokeStyleForCopy } from '../../interfaces/canvasStyles/strokeStyle';
+import { EasyStrokeStyle, StrokeStyle } from '../../interfaces/canvasStyles/strokeStyle';
 import AbstractPickerComponent from '../abstractPickerComponent';
-import { ALL_LINECAP, ALL_LINEJOIN, DEFAULT_LINECAP, DEFAULT_LINEJOIN, LineCap, LineJoin } from '../../interfaces/canvasStyles/styleTypes';
 import { FormsModule } from '@angular/forms';
 import { LoadingComponent } from "../../loading/loading.component";
+import { UniversalStrokeStyleComponent } from "../universal-stroke-style/universal-stroke-style.component";
 
 @Component({
   selector: 'app-easy-stroke-style',
   standalone: true,
   imports: [
-    FormsModule,
-    LoadingComponent
+    LoadingComponent,
+    UniversalStrokeStyleComponent
 ],
   templateUrl: './easy-stroke-style.component.html',
   styleUrl: './easy-stroke-style.component.scss'
@@ -20,137 +20,18 @@ export class EasyStrokeStyleComponent extends AbstractPickerComponent<Picker<Eas
 
   @Input({required: true}) public picker?: Picker<EasyStrokeStyle>;
 
-  public get lineJoin(): LineJoin {
-    return this.picker?.value?.lineJoin ?? DEFAULT_LINEJOIN;
-  }
-  
-  public set lineJoin(value: LineJoin) {
-    if (this.picker) {
-      if (this.picker.value === undefined) {
-        const ess = getEmptyEasyStrokeStyleForCopy();
-        ess.lineJoin = value;
-        this.picker.value = ess;
-      }
-      else {
-        this.picker.value.lineJoin = value;
-      }
-      this.onChange();
-    }
-  }
+  public strokeStylePicker?: Picker<StrokeStyle | EasyStrokeStyle>;
 
-  public get lineJoins(): LineJoin[] {
-    return ALL_LINEJOIN
-  }
-
-  public get lineCap(): LineCap {
-    return this.picker?.value?.lineCap ?? DEFAULT_LINECAP;
-  }
-  
-  public set lineCap(value: LineCap) {
-    if (this.picker) {
-      if (this.picker.value === undefined) {
-        const ess = getEmptyEasyStrokeStyleForCopy();
-        ess.lineCap = value;
-        this.picker.value = ess;
-      }
-      else {
-        this.picker.value.lineCap = value;
-      }
-      this.onChange();
-    }
-  }
-
-  get lineCaps(): LineCap[] {
-    return ALL_LINECAP
-  }
-  
-
-  public get lineDashOffsetEnabled(): boolean {
-    return this.picker?.value?.lineDashOffset !== undefined;
-  }
-  
-  public set lineDashOffsetEnabled(value: boolean) {
-    if (this.picker) {
-      if (this.picker.value === undefined) {
-        this.picker.value = getEmptyEasyStrokeStyleForCopy();
-      }
-      if (value !== this.lineDashOffsetEnabled) {
-        if (value) {
-          this.picker.value.lineDashOffset = this._lineDashOffset;
-        }
-        else {
-          this.picker.value.lineDashOffset = undefined;
-        }
-        this.onChange();
-      }
-    }
-  }
-
-  private _lineDashOffset: number = 0;
-
-  public get lineDashOffset(): number {
-    if (this.picker && this.picker.value && this.picker.value.lineDashOffset !== undefined) {
-      this._lineDashOffset = this.picker.value.lineDashOffset;
-    }
-    return this._lineDashOffset;
-  }
-
-  public set lineDashOffset(value: number) {
-    if (this.picker) {
-      this._lineDashOffset = value;
-
-      if (this.lineDashOffsetEnabled) {
-        if (this.picker.value === undefined) {
-          this.picker.value = getEmptyEasyStrokeStyleForCopy();
-        }
-        this.picker.value.lineDashOffset = value;
-      }
-
-      this.onChange();
-    }
-  }
-
-  private _lineDash: number[] = [];
-
-  public get lineDash(): number[] {
-    if (this.picker && this.picker.value && this.picker.value.lineDash) {
-      this._lineDash = this.picker.value.lineDash;
-    }
-    return this._lineDash;
-  }
-
-  public removeLineDash(index: number) {
-    if (index < this._lineDash.length) {
-      this._lineDash.splice(index, 1);
-      this.onChange();
-    }
-  }
-
-  public addLineDash() {
-    if (this.lineDashEnabled) {
-      this._lineDash.push(0);
-      this.onChange();
-    }
-  }
-
-  public get lineDashEnabled(): boolean {
-    return this.picker?.value?.lineDash !== undefined;
-  }
-
-  public set lineDashEnabled(value: boolean) {
-    if (this.picker) {
-      if (this.picker.value === undefined) {
-        this.picker.value = getEmptyEasyStrokeStyleForCopy();
-      }
-      if (value !== this.lineDashEnabled) {
-        if (value) {
-          this.picker.value.lineDash = this._lineDash;
-        }
-        else {
-          this.picker.value.lineDash = undefined;
-        }
-        this.onChange();
-      }
-    }
+  public ngAfterViewInit(): void {
+      setTimeout(() => {
+        this.strokeStylePicker = new Picker<StrokeStyle | EasyStrokeStyle>(() => this.picker?.value, (t: EasyStrokeStyle | StrokeStyle | undefined) => {
+          if (this.picker) {
+            this.picker.value = t as EasyStrokeStyle | undefined
+          }
+        }, true, () => this.picker?.isDisabled() ?? true);
+        this.strokeStylePicker.onValueChanged.addListener(() => {
+          this.onChange();
+        })
+      }, 0)
   }
 }
